7f91f17be89de7fe2fc68032c2cb2d0f20399800
mustafaergutt0 <mustafaergutt0@gmail.com>
2025-12-15 17:02:02 +0300

    FirstUpload

---
+++ src/test/java/com/ByteCraft/IotProject/IotProejctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ByteCraft.IotProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotProejctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
